c2fa8321ce35cafb2a1f2a71912dc7e7cf9e5624
skyuning <skyunlin@gmail.com>
2014-07-10 19:55:33 +0800

    Optimize code.

--- hostapp/src/main/java/com/example/hostapp/MainActivity.java
+++ hostapp/src/main/java/com/example/hostapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Instrumentation;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,15 +15,15 @@ import com.google.dexmaker.stock.ProxyBuilder;
 import org.skyun.aplugin.PluginInfo;
 import org.skyun.aplugin.PluginManager;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 
 public class MainActivity extends Activity {
 
-    private PluginInfo mPluginInfo;
+    private static String sPluginPackageName = "com.test.plugin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +38,48 @@ public class MainActivity extends Activity {
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hackInstrumentation();
+                InstrumentationHacker.hackInstrumentation(MainActivity.this);
+
+                if (InstrumentationHacker.getPluginInfoMap().get(sPluginPackageName) == null) {
+                    PluginInfo pluginInfo = PluginManager.testInstallPlugin(MainActivity.this, sPluginPackageName + ".apk");
+                    InstrumentationHacker.getPluginInfoMap().put(sPluginPackageName, pluginInfo);
+                }
+
+                try {
+                    Class clz = InstrumentationHacker.getPluginInfoMap().get(sPluginPackageName).mClassLoader.loadClass("com.test.plugin.MainActivity");
+                    Intent intent = new Intent(MainActivity.this, clz);
+                    MainActivity.this.startActivity(intent);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
     }
+}
 
-    private void hackInstrumentation() {
+class InstrumentationHacker {
+
+    private static HashMap<String, PluginInfo> sPluginInfoMap;
+
+    public static HashMap<String, PluginInfo> getPluginInfoMap() {
+        if (sPluginInfoMap == null)
+            sPluginInfoMap = new HashMap<String, PluginInfo>();
+        return sPluginInfoMap;
+    }
+
+    private static PluginInfo getPluginInfo(String activityClassName) {
+        for (String pluginPackageName : getPluginInfoMap().keySet()) {
+            if (activityClassName.startsWith(pluginPackageName))
+                return getPluginInfoMap().get(pluginPackageName);
+        }
+        return null;
+    }
+
+    public static void hackInstrumentation(final Activity activity) {
+        ProgressDialog mProgressDialog = new ProgressDialog(activity);
+        mProgressDialog.setMessage("hacking instrumentation ...");
+
         try {
             // 通过currentActivityThread静态方法获得ActivityThread对象.
             Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
@@ -53,68 +89,26 @@ public class MainActivity extends Activity {
             final Field instrumentationField = activityThreadClass.getDeclaredField("mInstrumentation");
             instrumentationField.setAccessible(true);
             final Instrumentation instrumentation = (Instrumentation) instrumentationField.get(activityThread);
-            if (instrumentation.getClass().getSimpleName().equals("Instrumentation_Proxy")) {
-                if (mPluginInfo == null)
-                    mPluginInfo = PluginManager.testInstallPlugin(this, "plugin.apk");
-
-                Class clz = mPluginInfo.mClassLoader.loadClass("com.test.plugin.MainActivity");
-                Intent intent = new Intent(this, clz);
-                startActivity(intent);
-                return;
+            if (instrumentation.getClass().getSimpleName().equals("Instrumentation")) {
+                mProgressDialog.show();
+                /*生成Instrumentation代理对象*/
+                Instrumentation minInstrumentation = ProxyBuilder
+                        .forClass(Instrumentation.class)
+                        .dexCache(activity.getDir("dx", Context.MODE_PRIVATE))
+                        .handler(new InstrumentationInvoker(instrumentation))
+                        .build();
+                        /*替换原来的Instrumentation对象.*/
+                instrumentationField.set(activityThread, minInstrumentation);
             }
 
-            AsyncTask<Void, Void, Void> asyncTask = new AsyncTask<Void, Void, Void>() {
-
-                private ProgressDialog mProgressDialog;
-
-                @Override
-                protected void onPreExecute() {
-                    mProgressDialog = new ProgressDialog(MainActivity.this);
-                    mProgressDialog.setMessage("hacking instrumentation ...");
-                    mProgressDialog.show();
-                }
-
-                @Override
-                protected Void doInBackground(Void... params) {
-                    try {
-                        /*生成Instrumentation代理对象*/
-                        Instrumentation minInstrumentation = ProxyBuilder
-                                .forClass(Instrumentation.class)
-                                .dexCache(getDir("dx", Context.MODE_PRIVATE))
-                                .handler(new InstrumentationInvoker(instrumentation))
-                                .build();
-                        /*替换原来的Instrumentation对象.*/
-                        instrumentationField.set(activityThread, minInstrumentation);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Void aVoid) {
-                    mProgressDialog.dismiss();
-                    if (mPluginInfo == null)
-                        mPluginInfo = PluginManager.testInstallPlugin(MainActivity.this, "plugin.apk");
-
-                    try {
-                        Class clz = mPluginInfo.mClassLoader.loadClass("com.test.plugin.MainActivity");
-                        Intent intent = new Intent(MainActivity.this, clz);
-                        startActivity(intent);
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            asyncTask.execute();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            mProgressDialog.dismiss();
         }
     }
 
-    private class InstrumentationInvoker implements InvocationHandler {
+    private static class InstrumentationInvoker implements InvocationHandler {
 
         private Instrumentation mInstrumentation;
 
@@ -129,7 +123,9 @@ public class MainActivity extends Activity {
                 return pluginActivity;
             } else if ("callActivityOnCreate".equals(method.getName())) {
                 Activity activity = (Activity) args[0];
-                PluginManager.replaceResources(activity, mPluginInfo.mDexPath);
+                PluginInfo pluginInfo = getPluginInfo(activity.getClass().getName());
+                if (pluginInfo != null)
+                    PluginManager.replaceResources(activity, pluginInfo.mDexPath);
                 return method.invoke(mInstrumentation, args);
             } else {
                 return method.invoke(mInstrumentation, args);
@@ -137,7 +133,11 @@ public class MainActivity extends Activity {
         }
 
         private Activity newActivity(ClassLoader cl, String className, Intent intent) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-            return (Activity) mPluginInfo.mClassLoader.loadClass(className).newInstance();
+            PluginInfo pluginInfo = getPluginInfo(className);
+            if (pluginInfo != null)
+                return (Activity) pluginInfo.mClassLoader.loadClass(className).newInstance();
+            else
+                return (Activity) cl.loadClass(className).newInstance();
         }
     }
 }
